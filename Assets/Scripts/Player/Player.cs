428dffcf20efd87f8f5580fc01df3ed1ff5b2353
Ben Matthew Pinto <benmattpinto@gmail.com>
2017-08-05 10:21:52 -0400

    Player movement is a go
    lmao

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	//Player's Color
 	public string m_color;
 
+	//Player's paddle width. Will change depending on power-ups.
 	public int m_playerWidth;
 
-	
+	//Player's movement speed. Will change depending on power-ups.
+	public float m_speed = 20f;
+
+	private Vector3 m_position = new Vector3(0f, 0,0f);
+
+	//Player's current power-up
+	//TODO:Fix dis
+	//public Powerup m_powerup;
 
 
 	// Use this for initialization
@@ -18,6 +27,9 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * m_speed);
+		m_position = new Vector3(xPos, 0, 0);
+		transform.position = m_position;
+	
 	}
 }
