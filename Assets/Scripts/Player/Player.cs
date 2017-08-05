4d0da43402d80e80e9b6ca97029abb76bda3ffc4
Ben Matthew Pinto <benmattpinto@gmail.com>
2017-08-05 10:42:07 -0400

    player snapping to the bottom of camera
    lmao

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour {
 	//Player's movement speed. Will change depending on power-ups.
 	public float m_speed = 20f;
 
-	private Vector3 m_position = new Vector3(0f, 0,0f);
+	private Vector3 m_position = new Vector3(0f, 0f, 0f);
 
 	//Player's current power-up
 	//TODO:Fix dis
@@ -22,14 +22,18 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * m_speed);
 		m_position = new Vector3(xPos, 0, 0);
+		m_position.y = Mathf.Clamp(this.gameObject.transform.position.y, 72.1f,72.1f);
+		m_position.z = Mathf.Clamp(this.gameObject.transform.position.z, -9.4f, -9.4f);
 		transform.position = m_position;
+
+		Debug.Log(m_position);
 	
 	}
 }
