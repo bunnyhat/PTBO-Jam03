1c2c470fb259c7c888533bb72f852f8a9ee7d79f
Ben Matthew Pinto <benmattpinto@gmail.com>
2017-08-05 09:51:45 -0400

    Player stuff
    lmao

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public string m_color;
+
+	public int m_playerWidth;
+
+	
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
