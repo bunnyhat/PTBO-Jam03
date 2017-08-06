0bf7411e73711e551d6c750d9207e50ff52c046b
Ben Matthew Pinto <benmattpinto@gmail.com>
2017-08-06 14:56:44 -0400

    main is up to date now
    lmao

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -27,6 +27,9 @@ public class Player : MonoBehaviour {
 	public Text m_scoreText;
 
 	public Slider m_slider;
+
+	public LerpDerp m_lerp;
+
 	
 
 	
@@ -42,6 +45,7 @@ public class Player : MonoBehaviour {
 		m_haveBall = true;
 		m_scoreText.text = m_score.ToString();
 		m_slider.value = m_score;
+		// m_lerp = this.GetComponent<LerpDerp>();
 
 		if(this.gameObject.tag == "Player 1") {
 			m_color = Color.red;
@@ -109,5 +113,10 @@ void Update () {
 
 		m_scoreText.text = m_score.ToString();
 		m_slider.value = m_score;
+		if(m_slider.value >= 38){
+			m_lerp.m_isBarFull = true;
+			m_lerp.BarFull();
+			
+		}
 	}
 }
