13d605a5d4e289a251c82e1371f8861c5c4b2c14
Ben Matthew Pinto <benmattpinto@gmail.com>
2017-08-06 11:14:50 -0400

    Created the exe to test build
    lmao

---
+++ Assets/Scripts/Screens/Font.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Font : MonoBehaviour {
+	public Text m_text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	m_text.color = Color.Lerp(Color.cyan, Color.magenta, Mathf.PingPong(Time.time, 1));
+	}
+}
---
+++ Assets/Scripts/Screens/SceneManage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour {
+
+	SceneManager m_sceneManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("A") == 1 || Input.GetAxis("A_2") == 1){
+			SceneManager.LoadScene("main");
+		}
+		
+	}
+}
